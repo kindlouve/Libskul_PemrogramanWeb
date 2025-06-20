f3f46b381c5bc4b55b5ba5423692b3f54b1c10ac
Al <151342993+allisyasyakdari@users.noreply.github.com>
2025-06-20 10:10:40 +0700

    hapus.php

--- backend/buku/hapus.php
+++ backend/buku/hapus.php
@@ -1,1 +1,20 @@
+<?php
+include '../koneksi.php';
 
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $sql = "DELETE FROM buku WHERE id=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id);
+
+    if ($stmt->execute()) {
+        header("Location: ../../admin/buku.php?status=deleted");
+        exit();
+    } else {
+        echo "Gagal menghapus buku!";
+    }
+} else {
+    echo "ID buku tidak ditemukan!";
+}
+?>
