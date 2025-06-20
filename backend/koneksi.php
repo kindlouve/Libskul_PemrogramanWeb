19aaf1d627723bc1ad4e89a0676f5876e25d625a
arra <151343947+kindlouve@users.noreply.github.com>
2025-06-20 11:06:33 +0700

    update koneksi

--- backend/koneksi.php
+++ backend/koneksi.php
@@ -1,1 +1,12 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db   = 'libskul';
 
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+?>
